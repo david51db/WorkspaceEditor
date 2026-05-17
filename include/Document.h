41fb1ec95948269c0fabd9239d18f17971f66023
david51db <davidgabrielpopa@gmail.com>
2026-05-17 14:00:48 +0300

    added document.h class

---
+++ include/Document.h
@@ -0,0 +1,36 @@
+
+
+#ifndef WORKSPACEEDITOR_DOCUMENT_H
+#define WORKSPACEEDITOR_DOCUMENT_H
+
+#include <vector>
+#include <string>
+
+enum class DocumentType {
+    Text,
+    ToDo
+};
+
+class Document {
+    std::string name;
+    std::string path;
+    DocumentType type;
+
+public:
+    Document(const std::string& name, const std::string& path, DocumentType type);
+    Document(const Document& obj)=default;
+    Document& operator=(const Document& obj)=default;
+    Document(Document&& obj)=default;
+    Document& operator=(Document&& obj)=default;
+    virtual ~Document()=default;
+
+    virtual void load()=0;
+    virtual void save() const=0;
+    virtual DocumentType getType() const=0;
+
+    const std::string& getName() const;
+    const std::string& getPath() const;
+
+};
+
+#endif //WORKSPACEEDITOR_DOCUMENT_H
