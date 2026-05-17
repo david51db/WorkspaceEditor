4c69cfa48e23bb061226cb5da32d2b59ada6ada5
david51db <davidgabrielpopa@gmail.com>
2026-05-17 13:48:31 +0300

    added buffer.h class

--- include/Buffer.h
+++ include/Buffer.h
@@ -1,8 +1,29 @@
-//
-// Created by David on 5/17/2026.
-//
 
 #ifndef WORKSPACEEDITOR_BUFFER_H
 #define WORKSPACEEDITOR_BUFFER_H
+#include <string>
+#include <vector>
+
+class Buffer {
+    std::vector<std::string> text;
+
+public:
+    Buffer();
+    Buffer(std::vector<std::string>& text);
+    Buffer(const Buffer& obj)=default;
+    Buffer& operator=(const Buffer& obj)=default;
+    Buffer(Buffer&&)=default;
+    Buffer& operator=(Buffer&&)=default;
+    ~Buffer()=default;
+
+    void insertLine(int pos, const std::string& line);
+    void deleteLine(int pos);
+    const std::string& getLine(int pos) const;
+    int getLineCount() const;
+    const std::vector<std::string>& getLines() const;
+    void clear();
+};
+
+
 
 #endif //WORKSPACEEDITOR_BUFFER_H
